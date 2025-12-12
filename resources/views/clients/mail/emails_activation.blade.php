435c9250be9fe603f7ae5bc82973c87ab5262811
sinhphan127 <vansinh1272003@gmail.com>
2025-12-12 21:21:53 +0700

    Add project

---
+++ resources/views/clients/mail/emails_activation.blade.php
@@ -0,0 +1,6 @@
+<p>Xin chào,</p>
+<p>Vui lòng nhấn vào liên kết dưới đây để kích hoạt tài khoản của bạn ở website Ecotour:</p>
+<button>
+    <a href="{{ $link }}">Kích hoạt tài khoản</a>
+</button>
+<p>Trân trọng,<br>C1SE.</p>
